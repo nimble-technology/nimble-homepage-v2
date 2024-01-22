3192deaaa9475383d20feac6719fbedf03ea5912
0xyoung-nimble <young@nimble.technology>
2024-01-22 21:42:10 +0800

    feat: add internal descriptions page

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import LandingSection from '../components/landing-section-component';
+import InternalDescriptions from '../components/internal-descriptions-component';
 
 const HomePage = () => {
   return (
@@ -8,8 +9,15 @@ const HomePage = () => {
       <Box id="landing-section">
         <LandingSection />
       </Box>
-      <Box id="section2" sx={{ height: '500px', background: 'lightgreen' }}>
-        Section 2 Content
+      <Box id="section2">
+      </Box>
+      <Box id="section3">
+        <InternalDescriptions/>
+      </Box>
+      <Box id="section4">
+      </Box>
+      <Box id="section5" sx={{ height: '500px', background: 'lightgreen' }}>
+        Section 5 Content
       </Box>
     </Box>
   );
