9317efb5d11c48073acd312c4252e62227e9cf32
0xyoung-nimble <152596496+0xyoung-nimble@users.noreply.github.com>
2024-01-23 01:25:42 +0800

    [PROD-990] Add values page (#5)
    * feat: add values page
    
    * add page-title component
    
    * update header click to values

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import LandingSection from '../components/landing-section-component';
+import ValueCardTop from '../components/value-card-top-component';
+import ValueCardBottom from '../components/value-card-bottom-component';
 
 const HomePage = () => {
   return (
     <Box>
       <Box id="landing-section">
         <LandingSection />
+      </Box>
+      <Box id="value-card">
+        <ValueCardTop />
+        <ValueCardBottom />
       </Box>
       <Box id="section2" sx={{ height: '500px', background: 'lightgreen' }}>
         Section 2 Content
