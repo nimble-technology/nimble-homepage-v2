8cb8df97f996f6800d12c6c5e26e8d46882c3e4d
0xyoung-nimble <young@nimble.technology>
2024-01-23 20:01:15 +0800

    Update the people page to mobile mode

--- src/components/people-card-item-component.js
+++ src/components/people-card-item-component.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Typography, Box, useTheme } from '@mui/material';
+import { useMobileContext } from '../mobileContext';
 
 const PeopleCardItem = ({ imageSrc, title, career, years_of_experience, previous_position, education, university }) => {
 
     const theme = useTheme();
+    const isMobile = useMobileContext();
 
     return (
         <Box>
@@ -13,14 +15,13 @@ const PeopleCardItem = ({ imageSrc, title, career, years_of_experience, previous
                 justifyContent: 'center',
                 alignItems: 'center',
             }}>
-                <Box component="img" src="/assets/avatar-circle.png" sx={{ width: 244, height: 245 }} />
-                <Box component="img" src={imageSrc} sx={{ width: 167, height: 168, position: 'absolute', borderRadius: '50%', objectFit: 'cover', }} />
+                <Box component="img" src="/assets/avatar-circle.png" sx={{ width: isMobile? '160px':'244px', height: isMobile? '160px':'245px' }} />
+                <Box component="img" src={imageSrc} sx={{ width: isMobile? '110px':'167px', height: isMobile? '110px':'168px', position: 'absolute', borderRadius: '50%', objectFit: 'cover', }} />
             </Box>
-
             <Typography
                 sx={{
                     fontFamily: "'Manrope', cursive",
-                    fontSize: '24px',
+                    fontSize: isMobile? '16px': '24px',
                     fontWeight: 700,
                     letterSpacing: 0,
                     textAlign: 'center',
@@ -32,7 +33,7 @@ const PeopleCardItem = ({ imageSrc, title, career, years_of_experience, previous
             <Typography
                 sx={{
                     fontFamily: "'Manrope', cursive",
-                    fontSize: '14px',
+                    fontSize: isMobile? '12px': '14px',
                     fontWeight: 400,
                     letterSpacing: 0,
                     textAlign: 'center',
