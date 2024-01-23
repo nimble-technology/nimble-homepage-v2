8facaffb4cb2958cfcfd27ee2b5468a1f793a7fb
0xyoung-nimble <young@nimble.technology>
2024-01-23 11:07:14 +0800

    update the name from nimbus to nimble

--- src/components/header-component.js
+++ src/components/header-component.js
@@ -38,7 +38,7 @@ const Header = () => {
                         marginRight: isMobile? '10px' : '30px'
                     }}
                 >
-                    Nimbus
+                    Nimble
                 </Typography>
 
 
