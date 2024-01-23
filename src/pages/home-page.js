7140fa9c8f7324e7fbcb4d4ffb603fd1b4169cd7
0xyoung-nimble <young@nimble.technology>
2024-01-23 22:06:09 +0800

    feat: add ecosystem partners page

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
+import EcosystemPartners from '../components/ecosystem-partners-component';
 import InternalDescriptions from '../components/internal-descriptions-component';
 import LandingSection from '../components/landing-section-component';
 import PeopleCard from '../components/people-card-component';
@@ -15,6 +16,9 @@ const HomePage = () => {
                 <LandingSection />
             </Box>
             <Box>
+                <EcosystemPartners />
+            </Box>
+            <Box>
                 <InternalDescriptions />
             </Box>
             <Box id={SECTIONS.VALUES}>
@@ -24,12 +28,12 @@ const HomePage = () => {
             <Box id="people-card">
                 <PeopleCard />
             </Box>
-       
+
             <Box id="section2" sx={{ height: '500px', background: 'lightgreen' }}>
                 Section 2 Content
             </Box>
         </Box>
-  );
+    );
 };
 
 export default HomePage;
