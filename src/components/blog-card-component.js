02e58d54df4005c60ff223d3db1be0a267a1ae1a
Anthony Isaacson <=>
2024-01-25 18:44:03 -0800

    target self

--- src/components/blog-card-component.js
+++ src/components/blog-card-component.js
@@ -18,7 +18,7 @@ const BlogCard = ({ title, date, fileName, thumb }) => {
         } else {
             urlPath = '/media/' + fileNameWithoutExtension;
         }
-        window.open(window.location.origin + urlPath);
+        window.open(window.location.origin + urlPath, '_self');
     };
 
     return (
